2ddfa0f7cc82571f1b3a886315cace19ee669400
Arjun Shaw <arjunshaw3325@gmail.com>
2026-03-20 00:10:34 +0530

    fixed mongo connection for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -7,11 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/studyos")
+// ✅ FIXED (use env variable)
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected"))
 .catch(err=>console.log(err));
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
